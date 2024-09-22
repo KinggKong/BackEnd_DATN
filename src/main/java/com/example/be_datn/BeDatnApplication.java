3f7a0445b7dda273666fc8fc0d7c86e4fce02499
quanghuy <quanghuy2231995@gmail.com>
2024-09-22 22:02:24 +0700

    init project

---
+++ src/main/java/com/example/be_datn/BeDatnApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_datn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDatnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDatnApplication.class, args);
+    }
+
+}
